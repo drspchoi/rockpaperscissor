2079dad40d42ac39916e4fe56e51be7dace55267
drspchoi <drspchoi@gmail.com>
2024-01-16 15:33:31 -0800

    Edit java.js It will show winner in game

--- java.js
+++ java.js
@@ -1,4 +1,4 @@
-/*function getComputerChoice () {
+function getComputerChoice () {
     let a=["rock", "paper", "scissor"];
     //console.log(a[(Math.floor(Math.random() * a.length))])//
     return a[(Math.floor(Math.random() * a.length))]
@@ -11,32 +11,54 @@ function playRound(playerSelection, computerSelection) {
     if (player===computer) {
         return (`player is ${player} and computer is ${computer}. Draw`)
     } else if (player=="rock" && computer=="scissor" || player=="paper" && computer=="rock" || player=="scissor" && computer=="paper") {
-        return (`player win. player: ${player} and computer: ${computer}`)
+        return "player win"
+        //return (`player win. player: ${player} and computer: ${computer}`)
     } else {
-        return (`computer win. player: ${player} and computer: ${computer}`)
+        return "computer win"
+        //return (`computer win. player: ${player} and computer: ${computer}`)
     }
 }
-*/
+
 function playerSelection() {
-    const rock = document.querySelector('#rock');
-    const paper= document.querySelector('#paper');
-    const scissor=document.querySelector('#scissor');
-    console.log(rock)
-    console.log(paper)
-    console.log(scissor)
+    const buttons = document.querySelectorAll('button');
+    console.log(buttons)
+    let player_win=0;
+    let computer_win=0;
+    let total_attemp=0;
+    let gameEnded = false;
+    buttons.forEach((button) => {
+        button.addEventListener('click',()=> {
+            const winner=playRound(button.id, getComputerChoice());
+            total_attemp+=1
+            if (winner=="player win") {
+                player_win+=1
+            } 
+            if (winner=="computer win") {
+                computer_win+=1
+            }
+            if (player_win===5 || computer_win===5) {
+                if (player_win>computer_win) {
+                winner_message("player", player_win,total_attemp)
+                }
+                else {
+                winner_message("computer", computer_win,total_attemp)    
+                }
+            gameEnded = true;
+            }
+        })
+    })
 }
 
-playerSelection()
-/*function game() {
-
-    console.log(playRound(playerSelection(),getComputerChoice()))
-    
-    console.log(playRound(playerSelection(),getComputerChoice()))
-    
-    console.log(playRound(playerSelection(),getComputerChoice()))
-    
-    console.log(playRound(playerSelection(),getComputerChoice()))
-    
-    console.log(playRound(playerSelection(),getComputerChoice()))  
+function winner_message(winner, howmany, total_attemp) {
+    const container=document.querySelector('div.result');
+    const message=document.createElement('div');
+    message.textContent=`${winner} won ${howmany} in ${total_attemp} attemps`;
+    container.appendChild(message)
+    gameEnded = true;
+}
+/*
+function game() {
+    console.log(playRound(button.id,getComputerChoice()))
 } 
 */
+playerSelection()
